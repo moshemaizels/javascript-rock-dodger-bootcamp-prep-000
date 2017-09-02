ba398207e55536fa55e241a478928d16b509d66f
Moshe Maizels <moshemaizels@gmail.com>
2017-09-02 21:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,21 +78,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     rock.style.top = `${top += 2}px`
-         var rockPos = rock.style.top.replace('px', '')
-         rockPos = parseInt(rockPos, 10)
-         //console.log(`rock position ${rock.style.top}`)
-         if(checkCollision(rock)) {
-           endGame()
-           alert("You Lose!")
-         } else {
 
-         if (rockPos > GAME_HEIGHT) {
-           //console.log(GAME)
-           rock.remove()
-           ROCKS.shift()
-         }
-         window.requestAnimationFrame(moveRock)
     /**
      * Otherwise, if the rock hasn't reached the bottom of
      * the GAME, we want to move it again.
@@ -108,7 +94,6 @@ function createRock(x) {
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
-  moveRock()
   ROCKS.push(rock)
 
   // Finally, return the rock element you've created
