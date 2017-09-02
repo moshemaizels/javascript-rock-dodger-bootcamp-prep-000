08d013811f3b6f932de99a7e7d48d2b42c49997d
Moshe Maizels <moshemaizels@gmail.com>
2017-09-02 21:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,6 +78,20 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     rock.style.top = `${top += 2}px`
+         var rockPos = rock.style.top.replace('px', '')
+         rockPos = parseInt(rockPos, 10)
+         //console.log(`rock position ${rock.style.top}`)
+         if(checkCollision(rock)) {
+           endGame()
+           alert("You Lose!")
+         } else {
+
+         if (rockPos > GAME_HEIGHT) {
+           //console.log(GAME)
+           rock.remove()
+           ROCKS.shift()
+         }
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
