3234b0a57230bba353f63dba4ece1c9b094c052a
Moshe Maizels <moshemaizels@gmail.com>
2017-09-02 21:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -125,6 +125,11 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval)
+  for (i = 0; i < ROCKS.length; i++) {
+   var currRock = ROCKS[i]
+   console.log(ROCKS)
+   currRock.remove()
+ }
 }
 
 function moveDodger(e) {
